f30efd2befa8cda512b9d8224c9b4017e600bea3
Linus Lee <linus@thesephist.com>
2019-06-11 23:29:27 -0700

    Allow packaging with rollup

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import postcss from 'rollup-plugin-postcss'
+
+const bundleNames = [
+    'blocks',
+    'reset',
+];
+
+const configs = bundleNames.map(name => {
+    return {
+        input: `src/${name}.css`,
+        output: {
+            file: `dist/${name}.min.css`,
+            format: 'es',
+        },
+        plugins: [
+            postcss({
+                minimize: true,
+                sourceMap: true,
+                extract: true,
+                modules: false,
+            }),
+        ],
+    }
+});
+
+export default configs
+
